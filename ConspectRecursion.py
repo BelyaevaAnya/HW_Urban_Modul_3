ca372a98f70569cdafda725778d232be10d38ab3
Anya_Belyaeva <71212278+BelyaevaAnya@users.noreply.github.com>
2024-07-27 17:56:04 +0300

    HW

---
+++ ConspectRecursion.py
@@ -0,0 +1,25 @@
+# def summa(n):
+#     if n == 0:
+#         return 0
+#     else:
+#         return n + summa(n - 1)
+#
+#
+# print(summa(5))
+#
+# stack = []
+# stack.append(1)
+# print('Add element:', stack)
+# stack.append(2)
+# print('Add element:', stack)
+# stack.append(3)
+# print('Add element:', stack)
+# print(stack)
+# stack.pop()
+# print('Pop element:', stack)
+# stack.pop()
+# print('Pop element:', stack)
+# stack.pop()
+# print('Pop element:', stack)
+
+# RecursionError: maximum recursion depth exceeded(ошибка связанная с невыходом из функции (рекурсия проверка))
